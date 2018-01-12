63f3987c934b5e50d22d4aa846092df7c8048b5b
leif <leiflyy@outlook.com>
2018-01-12 16:15:17 +0800

    增加 Redis 插件

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 use services\Mail;
-
+use services\Redis;
 /**
  * Created by PhpStorm.
  * User: EricLi
@@ -15,14 +15,21 @@ class HomeController extends BaseController
      *
      */
     public function home(){
-        $this->view = \services\View::make('home')->with('article',Article::first())
-            ->withTitle('DropLet :-D')
-            ->withKey('OK!')
-            ->withKeyKey('OK!');
+//        $this->view = \services\View::make('home')->with('article',Article::first())
+//            ->withTitle('DropLet :-D')
+//            ->withKey('OK!')
+//            ->withKeyKey('OK!');
 
 //        $this->mail = Mail::to(['1043283601@qq.com','leiflyy@outlook.com'])
-        $this->mail = Mail::to('1043283601@qq.com')
-            ->subject('Hello!!')
-            ->htmlBody('<h1>Hello~~~~~~~~</h1>');
+//        $this->mail = Mail::to('1043283601@qq.com')
+//            ->subject('Hello!!')
+//            ->htmlBody('<h1>Hello~~~~~~~~</h1>');
+
+
+        Redis::set('key','444',3000,'ms');
+        echo Redis::get('key');
+
     }
+
+
 }
---
+++ services/Redis.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EricLi
+ * Date: 2018/1/12
+ * Time: 15:33
+ */
+
+namespace services;
+
+
+use InvalidArgumentException;
+use Predis\Client;
+
+class Redis {
+    const CONFIG_FILE = '/config/redis.php';
+    protected static $redis;
+
+    public static function init()
+    {
+        self::$redis = new Client(require BASE_PATH.self::CONFIG_FILE);
+    }
+
+    public static function set($key,$value,$time=null,$unit=null)
+    {
+        self::init();
+        if ($time) {
+            switch ($unit) {
+                case 'h':
+                    $time *= 3600;
+                    break;
+                case 'm':
+                    $time *= 60;
+                    break;
+                case 's':
+                case 'ms':
+                    break;
+
+                default:
+                    throw new InvalidArgumentException('Units can only be "h", "m", "s" OR "ms"!');
+                    break;
+            }
+
+            if ($unit=='ms') {
+                self::_psetex($key,$value,$time);
+            } else {
+                self::_setex($key,$value,$time);
+            }
+
+        } else {
+
+            self::$redis->set($key,$value);
+
+        }
+    }
+
+    public static function get($key)
+    {
+        self::init();
+
+        return self::$redis->get($key);
+    }
+
+    public static function delete($key)
+    {
+        self::init();
+
+        return self::$redis->del($key);
+    }
+
+    private static function _setex($key,$value,$time)
+    {
+        self::$redis->setex($key,$time,$value);
+    }
+
+    private static function _psetex($key,$value,$time)
+    {
+        self::$redis->psetex($key,$time,$value);
+    }
+}
